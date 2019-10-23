c4034b3cabe5ed799305db4f4e97ecc4fed185c9
M-Porras <r.porras.avila@gmail.com>
2019-10-22 19:21:34 -0500

    Actializacion/Sesion-services

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -8,6 +8,11 @@ import { RegisterComponent } from './users/register/register.component';
 import { HomeComponent } from './home/home.component';
 import { WeAreComponent } from './we-are/we-are.component';
 import { ServicesComponent } from './services/services.component';
+import { Service01Component } from './services/service01/service01.component';
+import { Service02Component } from './services/service02/service02.component';
+import { Service03Component } from './services/service03/service03.component';
+import { Service04Component } from './services/service04/service04.component';
+import { Service05Component } from './services/service05/service05.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ViewProjectComponent } from './projects/view-project/view-project.component';
 import { ClientsComponent } from './clients/clients.component';
@@ -21,6 +26,11 @@ const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'weare', component: WeAreComponent },
     { path: 'services', component: ServicesComponent },
+    { path: 'service01', component: Service01Component },
+    { path: 'service02', component: Service02Component },
+    { path: 'service03', component: Service03Component },
+    { path: 'service04', component: Service04Component },
+    { path: 'service05', component: Service05Component },
     { path: 'projects', component: ProjectsComponent },
     { path: 'ViewProject', component: ViewProjectComponent },
     { path: 'clients', component: ClientsComponent },
@@ -40,6 +50,11 @@ export const routingComponents = [
     HomeComponent,
     WeAreComponent,
     ServicesComponent,
+    Service01Component,
+    Service02Component,
+    Service03Component,
+    Service04Component,
+    Service05Component,
     ProjectsComponent,
     ViewProjectComponent,
     ClientsComponent,
